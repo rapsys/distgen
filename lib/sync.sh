85ed1888b88e6f677577d598aadb4dc4e8f51b05
Raphaël Gertz <git@rapsys.eu>
2019-01-31 18:58:48 +0100

    Add mysql subvol to directories without COW

--- lib/sync.sh
+++ lib/sync.sh
@@ -5,4 +5,5 @@ rsync --delete -aAX $DISTCOOK/root/ ${MOUNTPOINT}/
 
 # Fix by hand /var/log/journal
 #XXX: see warning about copy on write on btrfs filesystem
-chattr +C ${MOUNTPOINT}/var/log/journal
+#XXX: disable cow on mysql directory as well
+chattr +C ${MOUNTPOINT}/var/log/journal ${MOUNTPOINT}/var/lib/mysql
