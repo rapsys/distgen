1143d2d2adbf4df9e2985d3e5fc31b5b9aa1fd6d
Raphaël Gertz <git@rapsys.eu>
2019-01-31 18:58:21 +0100

    Add slash, mail and mysql subvol

--- lib/root.sh
+++ lib/root.sh
@@ -6,14 +6,17 @@ mkdir -p ${MOUNTPOINT}
 # Mount slash filesystem
 mount /dev/mapper/${DATANAME} ${MOUNTPOINT}
 
-# Make boot in mount point
-mkdir -p ${MOUNTPOINT}/boot
+# Make boot, home, mail, mysql in mount point
+mkdir -p ${MOUNTPOINT}/{boot,home,var/spool/mail,var/lib/mysql}
 
 # Mount boot filesystem
 mount /dev/md/${MDBOOT} ${MOUNTPOINT}/boot
 
-# Make home in mount point
-mkdir -p ${MOUNTPOINT}/home
-
 # Mount home filesystem
 mount -o subvol=/home /dev/mapper/${DATANAME} ${MOUNTPOINT}/home
+
+# Mount mail filesystem
+mount -o subvol=/mail /dev/mapper/${DATANAME} ${MOUNTPOINT}/var/spool/mail
+
+# Mount mysql filesystem
+mount -o subvol=/mysql /dev/mapper/${DATANAME} ${MOUNTPOINT}/var/lib/mysql
